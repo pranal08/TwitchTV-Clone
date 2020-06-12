19aee764011882440a5a026e5bffd2a3d2632c5d
pranal08 <38245830+pranal08@users.noreply.github.com>
2020-06-12 17:26:45 -0400

    Routing Updates added

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { BrowserRouter, Route, Link} from 'react-router-dom';
+import StreamList from './streams/StreamList';
+import StreamEdit from './streams/StreamEdit';
+import StreamCreate from './streams/StreamCreate';
+import StreamShow from './streams/StreamShow';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () =>{
     return(
-        <div>App</div>
+        <div>
+            <BrowserRouter>
+            <div>
+                <Route path='/' exact component={StreamList}></Route>
+                <Route path='/new' exact component={StreamCreate}></Route>
+                <Route path='/edit' exact component={StreamEdit}></Route>
+                <Route path='/delete' exact component={StreamDelete}></Route>
+                <Route path='/show' exact component={StreamShow}></Route>
+            </div>
+            </BrowserRouter>
+        </div>
     );
 }
 
