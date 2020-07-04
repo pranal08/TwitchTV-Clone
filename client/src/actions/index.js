3816f84c7ceff195c47c1055eb36694ee00e1cf9
pranal08 <38245830+pranal08@users.noreply.github.com>
2020-07-04 12:29:17 -0400

    Rendering Stream List

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,3 +1,5 @@
+import streams from '../apis/streams';
+
 export const signIn = (userId) =>{
     return {
         type: 'SIGN_IN',
@@ -9,4 +11,35 @@ export const signOut = () =>{
     return {
         type: 'SIGN_OUT'
     };
+};
+
+export const createStream = (formvalues) => async (dispatch, getState) => {
+    const { userId }=getState().auth;
+    const response = await streams.post('/streams', {...formvalues, userId});
+
+    dispatch({ type: 'CREATE_STREAM', payload: response.data});
+};
+
+export const fetchStreams = () => async dispatch =>{
+    const response = await streams.get('/streams');
+
+    dispatch({ type: 'FETCH_STREAMS', payload: response.data});
+};
+
+export const fetchStream = (id) => async dispatch =>{
+    const response = await streams.get(`/streams/${id}`);
+
+    dispatch({ type: 'FETCH_STREAM', payload: response.data});
+};
+
+export const editStream = (id, formvalues) => async dispatch =>{
+    const response = await streams.put(`/streams/${id}`, formvalues);
+
+    dispatch({ type: 'EDIT_STREAM', payload: response.data});
+};
+
+export const deleteStream = (id) => async dispatch =>{
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({ type: 'DELETE_STREAM', payload: id});
 };
