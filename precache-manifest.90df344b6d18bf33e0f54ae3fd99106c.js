167e89c64e229c548a8f7aab8f5642d9a4698ebf
pranal08 <38245830+pranal08@users.noreply.github.com>
2020-07-09 17:21:20 -0400

    Updates

---
+++ precache-manifest.90df344b6d18bf33e0f54ae3fd99106c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5e87faa0a87e092269c0c9bc4bb75e0",
+    "url": "/TwitchTV-Clone/index.html"
+  },
+  {
+    "revision": "7c8cadbdae6c43710e85",
+    "url": "/TwitchTV-Clone/static/js/2.9b278231.chunk.js"
+  },
+  {
+    "revision": "7ff3e8849394329239d343acaff0f565",
+    "url": "/TwitchTV-Clone/static/js/2.9b278231.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96008b5f1a923aaa176",
+    "url": "/TwitchTV-Clone/static/js/main.e936238e.chunk.js"
+  },
+  {
+    "revision": "7a3b4b95afc421fe9453",
+    "url": "/TwitchTV-Clone/static/js/runtime-main.892003bc.js"
+  }
+]);
